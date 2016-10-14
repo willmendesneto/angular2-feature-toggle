e872af9a09e5341e7d6f40a799a5d0a3c63f4de2
Will Mendes <willmendesneto@gmail.com>
2016-10-14 23:37:54 +1100

    refactor(featureToggle): chaging approach in tests (#1)

--- src/app/feature-toggle.directive.spec.ts
+++ src/app/feature-toggle.directive.spec.ts
@@ -1,54 +1,43 @@
 /* tslint:disable:no-unused-variable */
 
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { FeatureToggleDirective } from './feature-toggle.directive';
 
 @Component({
   selector: 'kp-container',
-  template: `<div *kpFeatureToggle="true">Feature toggle enabled</div>`,
+  template: `
+  <div>
+    <div class="feature-toggle-component" *kpFeatureToggle="true">Feature toggle enabled</div>
+    <div class="feature-toggle-component" *kpFeatureToggle="false">Feature toggle disabled</div>
+  </div>
+  `
 })
 class ContainerComponent {}
-
-@Component({
-  selector: 'kp-disabled-container',
-  template: `<div *kpFeatureToggle="false">Feature toggle disabled</div>`,
-})
-class DisabledContainerComponent {}
 
 describe('Directive: FeatureToggle', () => {
 
   let fixture;
 
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [ ContainerComponent, FeatureToggleDirective ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .createComponent(ContainerComponent);
+    fixture.componentInstance.isEnabled = true;
+    fixture.detectChanges();
+  });
+
   describe('When featureToggle is enabled', () => {
-
-    beforeEach(() => {
-      fixture = TestBed.configureTestingModule({
-        declarations: [ ContainerComponent, FeatureToggleDirective]
-      })
-      .createComponent(ContainerComponent);
-      fixture.componentInstance.isEnabled = true;
-      fixture.detectChanges();
-    });
-
     it('should render the component content', async(() => {
-      expect(fixture.nativeElement.querySelector('div').innerText).toBe(`Feature toggle enabled`);
+      expect(fixture.nativeElement.querySelectorAll('.feature-toggle-component')[0].innerText).toBe(`Feature toggle enabled`);
     }));
   });
 
   describe('When featureToggle is enabled', () => {
-
-    beforeEach(() => {
-      fixture = TestBed.configureTestingModule({
-        declarations: [ DisabledContainerComponent, FeatureToggleDirective]
-      })
-      .createComponent(DisabledContainerComponent);
-      fixture.componentInstance.isEnabled = false;
-      fixture.detectChanges();
-    });
-
     it('should NOT render the component content', async(() => {
-      expect(fixture.nativeElement.querySelector('div')).toBe(null);
+      expect(fixture.nativeElement.querySelectorAll('.feature-toggle-component')[1]).toBe(undefined);
     }));
   });
 });
